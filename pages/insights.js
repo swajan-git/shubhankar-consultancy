becbb93b93dbaae65186727fc2b01ba05bbcea62
SHAHJAHAN MD. SWAJAN <swajan.mis13.du@gmail.com>
2021-06-22 23:47:54 +0600

    insight page

---
+++ pages/insights.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const insights = () => {
+    return (
+        <>
+            <div className="jumbotron text-center">
+                Insights
+            </div>
+        </>
+    );
+};
+
+export default insights;
