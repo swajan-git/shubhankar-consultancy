f0cec550136031ee617a9237235d666f9dd3cca4
SHAHJAHAN MD. SWAJAN <swajan.mis13.du@gmail.com>
2021-06-24 21:40:40 +0600

    scroll sticky top done

--- components/ServicePages/ServicePageScroll.js
+++ components/ServicePages/ServicePageScroll.js
@@ -10,22 +10,22 @@ const ServicePageScroll = () => {
                     <div className="row">
                         <div className="col-md-3">
                             <div className={`scroll-item w-75 ${activeSection == 1 && "active"}`}>
-                                <a onClick={() => setActiveSection(1)} href="#bpo" className={`service-section-scroll  ${activeSection == 1 && "active"}`}>BPO Services {activeSection == 1 && "ac"} </a>
+                                <a onClick={() => setActiveSection(1)} href="#intro" className={`service-section-scroll  ${activeSection == 1 && "active"}`}>BPO Services </a>
                             </div>
                         </div>
                         <div className="col-md-3">
                             <div className={`scroll-item w-75 ${activeSection == 2 && "active"}`}>
-                                <a onClick={() => setActiveSection(2)} href="#bpo" className={`service-section-scroll  ${activeSection == 2 && "active"}`}>BPO Services {activeSection == 2 && "ac"} </a>
+                                <a onClick={() => setActiveSection(2)} href="#case-studies" className={`service-section-scroll  ${activeSection == 2 && "active"}`}>Case Studies </a>
                             </div>
                         </div>
                         <div className="col-md-3">
                             <div className={`scroll-item w-75 ${activeSection == 3 && "active"}`}>
-                                <a onClick={() => setActiveSection(3)} href="#bpo" className={`service-section-scroll  ${activeSection == 3 && "active"}`}>BPO Services {activeSection == 3 && "ac"} </a>
+                                <a onClick={() => setActiveSection(3)} href="#we-think" className={`service-section-scroll  ${activeSection == 3 && "active"}`}>What We Think </a>
                             </div>
                         </div>
                         <div className="col-md-3">
                             <div className={`scroll-item w-75 ${activeSection == 4 && "active"}`}>
-                                <a onClick={() => setActiveSection(4)} href="#bpo" className={`service-section-scroll  ${activeSection == 4 && "active"}`}>BPO Services {activeSection == 4 && "ac"} </a>
+                                <a onClick={() => setActiveSection(4)} href="#blogs" className={`service-section-scroll  ${activeSection == 4 && "active"}`}>Blogs </a>
                             </div>
                         </div>
                     </div>
