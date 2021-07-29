4e9698e7fcb101b591597b4bab7afae3e9489db9
SHAHJAHAN MD. SWAJAN <swajan.mis13.du@gmail.com>
2021-07-29 17:08:44 +0600

    banking page done

---
+++ pages/industries/banking-and-leasing.js
@@ -0,0 +1,75 @@
+import SEOLayout from "../../components/shared/SEOLayout";
+
+
+const IndustriesPage = () => {
+    return (
+        <>
+            <SEOLayout title="Banking and Leasing | Shubhankar Consulting" />
+            <main className="banking">
+                <section className="bg-section">
+                    <div className="bg-blue">
+                        <div className="py-5"></div>
+                        <div className="container-fluid">
+                            <div className="row">
+                                <div className="col-md-3"></div>
+                                <div className="col-md-5 ">
+                                    <h1 className="display-4 text-white  mt-md-5 font-weight-900 pb-3 ">
+                                        Banking and <br /> Leasing<span className="orangered-dot">.</span>
+                                    </h1>
+                                </div>
+                            </div>
+                            <div className="row">
+                                <div className="col-md-3 orange-top orange-right py-4"></div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="container-fluid">
+                        <div className="row">
+                            <div className="col-md-3 orange-right"></div>
+                            <div className="col-md-5 py-5">
+                                <p className="font-20 text-white">
+                                Bangladesh is a developing agricultural country that is steadily becoming more industrialized. The people of this subcontinent have traditionally been wary of business and trade. However, as time passes, people are inspired and motivated to take risks and start businesses. Entrepreneurs confront a scarcity of starting cash, and if they have managed to start a business, they face a severe lack of working capital. They seek long-term direct financing from commercial banks, specialized banks, and other financial institutions in this situation, however obtaining long-term loans from direct financing institutions in Bangladesh is difficult. Leasing business in Bangladesh is providing for the alternative financing introduced by the prominent leasing companies in Bangladesh.
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                </section>
+                <section>
+                    <div className="container-fluid">
+                        <div className="row">
+                            <div className="col-md-3 orange-right"></div>
+                            <div className="col-md-6 py-5">
+                                <h3 className="font-36 mb-4 text-dark font-weight-900">
+                                    Capabilities
+                                </h3>
+                            </div>
+                            <div className="col-md-3"></div>
+                        </div>
+                        <div className="row ">
+                            <div className="col-md-3 "></div>
+                            <div className="col-md-1 orange-right orange-top"></div>
+                            <div className="col-md-5">
+                                <h3>Comprehensive Understanding of Regulations</h3>
+                                <p>
+                                We understand legal limits, various accounting standards across nations, cultural variances, client expectations, and the obstacles that CIOs and CFOs face in making the best business decisions.
+                                </p>
+
+
+                                <h3 className="mb-4 mt-4">Guidance from Scratch</h3>
+                                <p>
+                                From the definition of the strategy through the implementation of the solution, we are with our clients every step of the way.
+                                </p>
+                            </div>
+                            <div className="col-md-3 orange-bottom orange-top orange-left mt-5"></div>
+                        </div>
+                        <div className="row mb-5">
+                            <div className="col-md-4 orange-top"></div>
+                        </div>
+                    </div>
+                </section>
+            </main>
+        </>
+    );
+};
+
+export default IndustriesPage;
---
+++ pages/industries/healthcare.js
@@ -0,0 +1,78 @@
+import SEOLayout from "../../components/shared/SEOLayout";
+
+
+const IndustriesPage = () => {
+    return (
+        <>
+            <SEOLayout title="Healthcare | Shubhankar Consulting" />
+            <main className="healthcare">
+                <section className="bg-section">
+                    <div className="py-5"></div>
+                    <div className="container-fluid">
+                        <div className="row">
+                            <div className="col-md-3 orange-top orange-right"></div>
+                            <div className="col-md-6 text-white">
+                                <h1 className="display-4  mt-md-5 font-weight-900 pb-3 ">
+                                    Healthcare <span className="orangered-dot">.</span>
+                                </h1>
+                                <p className="font-18">
+                                    With Insight Driven Health, we help clients offer more effective, efficient, and affordable healthcare from the back office to the doctor's office.
+                                </p>
+                                <p className="font-18">
+                                    Cloud was all about aspiration the day before yesterday. It's all about scale these days.
+                                </p>
+                                <p className="font-18">
+                                    Cloud is a disruptive force in the industry, driving innovation. Cloud-based healthtech companies continue to gain clout as industry influencers. Furthermore, the main three cloud hyperscalers in the North American market—Microsoft, Amazon, and Google—have healthcare in mind and are investing in research and development.
+                                </p>
+                            </div>
+                            <div className="col-md-3 bg-transparent-blue"></div>
+                        </div>
+                    </div>
+                </section>
+                <section>
+                    <div className="container-fluid">
+                        <div className="row">
+                            <div className="col-md-3 orange-right"></div>
+                            <div className="col-md-6 py-5">
+                                <h3 className="font-36 mb-4 text-dark font-weight-900">
+                                    Capabilities
+                                </h3>
+                            </div>
+                            <div className="col-md-3 bg-transparent-blue"></div>
+                        </div>
+                        <div className="row ">
+                            <div className="col-md-3 orange-right"></div>
+                            <div className="col-md-6 offset-md-1 pb-5">
+                                <h3>Digital Health</h3>
+                                <p>
+                                    Innovative digital health methods are being implemented by health organizations. Leading firms can fundamentally transform the way healthcare is delivered and the health consumer experience by focusing on cloud, EHRs, machine learning, AI, cybersecurity, and other technologies.
+                                </p>
+
+
+                                <h3 className="mb-4">Customer Experience</h3>
+                                <p>
+                                    The human perspective is becoming increasingly important as the need for virtual care, home health solutions, retail care, self-service, and patient transparency grows. The organizations that concentrate on providing meaningful experiences on an individual's terms will be the ones to lead. This is not an easy task. To deliver care, you must discover the correct operating model, technology, data, and culture.
+                                </p>
+                                <p>
+                                    We blend technology and human ingenuity to help consumers better understand their needs and take advantage of technology-based customisation. Our talented team combines deep data with healthcare marketing, customer service, omnichannel patient access, and goods.
+                                </p>
+
+
+                                <h3 className="my-4">
+                                    Operational Transformation
+                                </h3>
+                                <p>
+                                    Every change in the market affects how a company operates and performs, often dramatically. Operations transformation (OT) can help your business or organization deal with expanding markets, increasing global competition, rising customer expectations, advanced technologies, and increasing digitization. You'll be equipped to deal with any potential hurdles if you adapt your operations.
+                                </p>
+
+                            </div>
+                            <div className="col-md-2 orange-top orange-left mt-5"></div>
+                        </div>
+                    </div>
+                </section>
+            </main>
+        </>
+    );
+};
+
+export default IndustriesPage;
