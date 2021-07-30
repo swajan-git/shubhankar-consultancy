c705665742e21215933a8fb55fd9bb322501f589
SHAHJAHAN MD. SWAJAN <swajan.mis13.du@gmail.com>
2021-07-30 12:17:48 +0600

    capital market and jpharmaceuticals updated

--- pages/industries/capital-markets-wealth-management.js
+++ pages/industries/capital-markets-wealth-management.js
@@ -10,16 +10,16 @@ const IndustriesPage = () => {
                 <section className="bg-section">
                     <div className="container-fluid">
                         <div className="row">
-                            <div className="col-md-8 py-5 offset-md-2 bg-white">
+                            <div className="col-md-6 py-5 offset-md-3 bg-white">
                                 <h1 className="display-4 font-dark mt-md-5 font-weight-900 pb-3 ">
-                                    Capital Market & Wealth <br />Management <span className="orangered-dot">.</span>
+                                    Capital Market & Wealth Management <span className="orangered-dot">.</span>
                                 </h1>
                                 <p className="font-18">We help investment banks, asset and wealth managers, and exchanges prepare for the digital future.</p>
                             </div>
                         </div>
                         <div className="row">
-                            <div className="col-md-2 orange-top orange-right bg-transparent-blue "></div>
-                            <div className="col-md-8 bg-transparent-blue py-5">
+                            <div className="col-md-3 orange-top orange-right bg-transparent-blue "></div>
+                            <div className="col-md-6 bg-transparent-blue py-5">
                                 <h3 className="font-36 text-white mb-3 font-weight-900">
                                     Ready for digital enabled capital Markets
                                 </h3>
@@ -32,25 +32,25 @@ const IndustriesPage = () => {
                             </div>
                         </div>
                         <div className="row">
-                            <div className="col-md-2 bg-white orange-right"></div>
-                            <div className="col-md-8 bg-white pt-5">
+                            <div className="col-md-3 bg-white orange-right"></div>
+                            <div className="col-md-6 bg-white pt-5">
                                 <h3 className="font-36 mb-4 text-dark font-weight-900">
                                     Capabilities
                                 </h3>
                             </div>
                         </div>
                         <div className="row">
-                            <div className="col-md-2 orange-bottom bg-white"></div>
+                            <div className="col-md-3 orange-bottom bg-white"></div>
                             <div className="col-md-1 orange-bottom bg-white orange-top orange-right"></div>
-                            <div className="col-md-7 bg-white">
-                                <h3>Asset Management</h3>
+                            <div className="col-md-5 bg-white">
+                                <h3 className="py-4 mt-5">Asset Management</h3>
                                 <p>
                                     Reinvigorated. Transformed. Asset managers now confront a variety of challenges and opportunities, including bending the cost curve, identifying new sources of alpha, reinventing operating models, and boosting returns. Optimal capital spending, data-driven analytics, AI-enhanced insights, and agile, adaptable real-time operations are now required for success.
                                 </p>
                                 <p>
                                     We work with asset managers, institutional investors, and asset servicers to help them improve their business models and expand faster. We help businesses prosper in today's markets by streamlining and integrating front, middle, and back office activities and specialized platforms, boosting risk posture and data delivery, and using disruptive breakthroughs like AI, predictive analytics, and distributed ledgers.
                                 </p>
-                                <h3 className="mb-4">Wealth Management</h3>
+                                <h3 className="py-4 mt-5">Wealth Management</h3>
                                 <p>
                                     Clients today want a whole new level of service that is more educated, personalized, and transparent than ever before, and they expect it faster than ever before. Companies that rise to the digital challenge use industry data to restructure their businesses from the ground up and aim for operational excellence—and many won't go it alone.
                                     We create end-to-end solutions for premier wealth management firms by using our deep industry experience, broad range of capabilities, and synergies across our whole organization.
@@ -62,19 +62,19 @@ const IndustriesPage = () => {
                 <section className="bg-white py-5">
                     <div className="container-fluid">
                         <div className="row">
-                            <div className="col-md-6 offset-md-2">
-                                <h3>Investment Management</h3>
+                            <div className="col-md-6 offset-md-3">
+                                <h3 className="py-4 mt-5">Investment Management</h3>
                                 <p>
                                     Regardless of your strategy approach, any future-ready organization will need a digitally connected operating model to keep costs low while providing better experiences for customers and staff.
                                 </p>
                                 <p>
                                     Using our market-relevant skills, we develop services and solutions that can assist investment banks in transforming and seeking competitive advantages.
                                 </p>
-                                <h3 className="mb-4">Market Infrastructure</h3>
+                                <h3 className="py-4 mt-5">Market Infrastructure</h3>
                                 <p>
                                     We work with exchanges, clearing houses, central securities depositories, and custodians to build long-term strategies, technology, and operations.                                </p>
                             </div>
-                            <div className="col-md-4 orange-top orange-left orange-bottom"></div>
+                            <div className="col-md-3 orange-top orange-left orange-bottom"></div>
                         </div>
                     </div>
                 </section>
--- pages/industries/pharmaceuticals-chemicals.js
+++ pages/industries/pharmaceuticals-chemicals.js
@@ -12,12 +12,16 @@ const IndustriesPage = () => {
                         <div className="row">
                             <div className="col-md-3 orange-top orange-right"></div>
                             <div className="col-md-6 text-white">
-                                <h1 className="display-4  mt-md-5 font-weight-900 pb-3 ">
-                                    Capital Market & Wealth <br />Management <span className="orangered-dot">.</span>
+                                <h1 className="display-4  font-weight-900 pb-3 ">
+                                    Pharmaceuticals & <br />Chemicals <span className="orangered-dot">.</span>
                                 </h1>
-                                <p className="font-fold font-20 py-4">The future is now</p>
-                                <p className="font-18">We help investment banks, asset and wealth managers, and exchanges prepare for the digital future.</p>
-                                <p className="font-18">We help investment banks, asset and wealth managers, and exchanges prepare for the digital future.</p>
+                                <p className="font-weight-900 font-20 py-4">The future is now</p>
+                                <p className="font-18 mb-4">
+                                    We use innovation, transformational strategies, and digital enablement for growth, differentiation, and resilient operations by combining our chemical consulting and technology experience with the current chemical industry trends.
+                                </p>
+                                <p className="font-18">
+                                    We're helping to change the way businesses operate, whether it's through mergers and acquisitions, streamlining factory operations, or using SAP S/4HANA for business transformation.
+                                </p>
                             </div>
                             <div className="col-md-3 bg-transparent-blue"></div>
                         </div>
@@ -37,28 +41,28 @@ const IndustriesPage = () => {
                         <div className="row ">
                             <div className="col-md-3 orange-right"></div>
                             <div className="col-md-6 offset-md-1 pb-5">
-                                <h3>Production & Operations </h3>
+                                <h3 className="py-4">Production & Operations </h3>
                                 <p>
                                     To thrive in a world of increasing economic, environmental, and regulatory challenges,
                                     they must increase efficiency, adaptability, and safety. They must go beyond single-site “Industry 4.0” digitization efforts to optimize their whole production, personnel, and supply chain networks in order to obtain the visibility and control they require.
                                 </p>
 
-                                <h3 className="mb-4">Supply Chain & Operations</h3>
+                                <h3 className="py-4 mt-5">Supply Chain & Operations</h3>
                                 <p>
-                                construct a customer-centric supply chain that works more quickly and with greater agility To improve efficiency by using a zero-based optimization strategy. Greater resilience, sustainability, openness, and trust will be used to power future growth. And to use the transformative potential of data and digital technology to empower people to operate differently and better.
+                                    construct a customer-centric supply chain that works more quickly and with greater agility To improve efficiency by using a zero-based optimization strategy. Greater resilience, sustainability, openness, and trust will be used to power future growth. And to use the transformative potential of data and digital technology to empower people to operate differently and better.
                                 </p>
                                 <p>
-                                The intelligent supply chain is what we call it: an engine of development through novel consumer experiences, a generator of profitability in difficult economic times, and a responsible source of stakeholder trust in the post-COVID world, allowing competitiveness.
+                                    The intelligent supply chain is what we call it: an engine of development through novel consumer experiences, a generator of profitability in difficult economic times, and a responsible source of stakeholder trust in the post-COVID world, allowing competitiveness.
                                 </p>
-                                <h3 className="my-4">
+                                <h3 className="py-4 mt-5">
                                     Sustainability
                                 </h3>
                                 <p>
-                                Organizations must now demonstrate that they are purposeful about sustainability, hold strong ethical standards, and operate responsibly in everything they do as stakeholders demand more environmental, social, and governance (ESG) efforts, regulations tighten, and consumers increasingly expect brands to take action.
+                                    Organizations must now demonstrate that they are purposeful about sustainability, hold strong ethical standards, and operate responsibly in everything they do as stakeholders demand more environmental, social, and governance (ESG) efforts, regulations tighten, and consumers increasingly expect brands to take action.
 
                                 </p>
                                 <p className="pb-5">
-                                Companies and brands are increasingly looking to their partners—as well as technology and innovation—to help them incorporate sustainability and make substantial changes that benefit business, society, and the environment. Our industry-experienced teams develop and implement strategies, operating models, procedures, and technology to assist organizations in achieving their sustainable objectives.
+                                    Companies and brands are increasingly looking to their partners—as well as technology and innovation—to help them incorporate sustainability and make substantial changes that benefit business, society, and the environment. Our industry-experienced teams develop and implement strategies, operating models, procedures, and technology to assist organizations in achieving their sustainable objectives.
                                 </p>
                             </div>
                             <div className="col-md-2 orange-top orange-left mt-5"></div>
