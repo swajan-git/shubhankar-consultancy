5cc4a154274df1e5baa6391131c87ec59431d74c
SHAHJAHAN MD. SWAJAN <swajan.mis13.du@gmail.com>
2021-08-09 16:01:02 +0600

    energy page structure done

---
+++ pages/industries/energy.js
@@ -0,0 +1,92 @@
+import SEOLayout from "../../components/shared/SEOLayout";
+
+
+const IndustriesPage = () => {
+    return (
+        <>
+            <SEOLayout title="Energy | Industries" />
+            <main className="energy">
+                <section className="bg-section">
+                    <div className="container-fluid large-top-pad">
+                        <div className="row">
+                            <div className="col-md-3 orange-top orange-right"></div>
+                            <div className="col-md-6 mb-5 text-white">
+                                <h1 className="display-4 text-white mb-4  font-weight-900 pb-3 ">
+                                    Energy <span className="orangered-dot">.</span>
+                                </h1>
+                                <p className="font-18">
+                                    Lorem ipsum dolor sit, amet consectetur adipisicing elit. Similique labore aliquam consequatur nesciunt, quaerat eius modi, ad veritatis ipsa fugiat eos eveniet animi sed. Odio veritatis totam officiis sed, rem vel! Vero, laudantium. Natus facere a minima neque earum ut quod esse reiciendis repudiandae modi? Doloremque quasi quas saepe pariatur minima ratione repellendus, quibusdam explicabo!
+                                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem inventore minus tenetur! Officiis iste perferendis dicta doloribus ea modi odit? Soluta itaque iste maxime rerum repudiandae aliquid deleniti mollitia nihil hic aperiam!
+                                </p>
+                                <p className="font-18 mt-4 mb-5">
+                                    Lorem ipsum dolor sit amet consectetur, adipisicing elit. Officia voluptatibus nostrum repellendus esse. Quas aliquid aperiam omnis ad, nulla sunt consequuntur fugit facilis veniam maxime eligendi illo incidunt, explicabo recusandae at consequatur!
+                                </p>
+                            </div>
+                            <div className="col-md-3 bg-transparent-blue"></div>
+                        </div>
+                        <div className="row">
+                            <div className="col-md-3 bg-white orange-right"></div>
+                            <div className="col-md-6 bg-white px-5 pt-5">
+                                <h3 className="font-36 mb-5 text-dark font-weight-900">
+                                    Capabilities
+                                </h3>
+                                <div className="pl-md-5">
+                                    <div className="cap-info">
+                                        <h3 className="text-dark ">
+                                            Program Development
+                                        </h3>
+                                        <p className="font-18">
+                                            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eveniet aut at veniam alias saepe, sit necessitatibus iste, sequi illum numquam quam vitae!
+                                        </p>
+                                    </div>
+                                    <div className="cap-info">
+                                        <h3 className="text-dark ">
+                                            Budgeting
+                                        </h3>
+                                        <p className="font-18">
+                                            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eveniet aut at veniam alias saepe, sit necessitatibus iste, sequi illum numquam quam vitae!
+                                        </p>
+                                    </div>
+                                </div>
+                                
+                            </div>
+                            <div className="col-md-3 bg-transparent-blue">
+                                <div className="mx-5 px-4">
+                                    <img src="/img/industries/energy/thunder.png" alt="" className="img-fluid" />
+                                </div>
+                            </div>
+                        </div>
+                        <div className="row mb-5">
+                            <div className="col-md-3 bg-white orange-right orange-bottom"></div>
+                            <div className="col-md-6 bg-white px-5 ">
+                               
+                                <div className="pl-md-5">
+                                    <div className="cap-info">
+                                        <h3 className="text-dark ">
+                                            Program Development
+                                        </h3>
+                                        <p className="font-18">
+                                            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eveniet aut at veniam alias saepe, sit necessitatibus iste, sequi illum numquam quam vitae!
+                                        </p>
+                                    </div>
+                                    <div className="cap-info">
+                                        <h3 className="text-dark ">
+                                            Budgeting
+                                        </h3>
+                                        <p className="font-18">
+                                            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eveniet aut at veniam alias saepe, sit necessitatibus iste, sequi illum numquam quam vitae!
+                                        </p>
+                                    </div>
+                                </div>
+                                
+                            </div>
+                        <div className="col-md-3 bg-white"></div>
+                        </div>
+                    </div>
+                </section>
+            </main>
+        </>
+    );
+};
+
+export default IndustriesPage;
