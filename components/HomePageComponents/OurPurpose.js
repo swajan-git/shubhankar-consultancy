ae21a2568e056fa94b776bbfde5cbc3cb26ea4ca
SHAHJAHAN MD. SWAJAN <swajan.mis13.du@gmail.com>
2021-08-11 04:36:16 +0600

    logo section structure done

---
+++ components/HomePageComponents/OurPurpose.js
@@ -0,0 +1,37 @@
+
+const OurPurpose = () => {
+    return (
+        <>
+            <section className="our-purpose py-5">
+                <div className="container my-4">
+                    <div className="row">
+                        <div className="col-md-8 text-center offset-md-2">
+                            <h1 className="font-weight-900 mb-4">
+                                Our Purpose
+                            </h1>
+                            <p className="font-18">
+                                Shubhankar Consulting assists clients in growing and transforming their organizations by designing diverse and personalized business policies and procedures, and then integrating them with technology, strategy, and operational excellence.
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </section>
+            {/* video section */}
+            <section className="logo-video-bg py-5 bg-secondary d-flex align-items-center text-white">
+                <video loop muted autoPlay="autoplay" >
+                    <source src="https://firebasestorage.googleapis.com/v0/b/shubhankar-consulting.appspot.com/o/videos%2F4)%20Behind%20Shubhankar%20Logo.mp4?alt=media&token=48fd1b11-867a-4adb-860b-ee759b88831e" type="video/mp4" ></source>
+                </video>
+                <div className="overlay"></div>
+                <div className="container content">
+                    <div className="row">
+                        <div className="col-md-8 offset-md-2 text-center">
+                            <img src="/img/home/icons/bulb.png" alt="" className="logo-white" />
+                        </div>
+                    </div>
+                </div>
+            </section>
+        </>
+    );
+};
+
+export default OurPurpose;
--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,7 @@ import Head from 'next/head'
 import Image from 'next/image'
 import CoreValues from '../components/HomePageComponents/CoreValues'
 import FeaturedClients from '../components/HomePageComponents/FeaturedClients'
+import OurPurpose from '../components/HomePageComponents/OurPurpose'
 import ReimagineYourCareer from '../components/HomePageComponents/ReimagineYourCareer'
 import YouTubeResponsiveEmbed from '../components/shared/YouTubeResponsiveEmbed'
 import { siteLink } from '../shared/data'
@@ -16,20 +17,7 @@ export default function Home() {
             </Head>
 
             <main className="home-page">
-                <section className="our-purpose py-5">
-                    <div className="container my-4">
-                        <div className="row">
-                            <div className="col-md-8 text-center offset-md-2">
-                                <h1 className="font-weight-900 mb-4">
-                                    Our Purpose
-                                </h1>
-                                <p className="font-18">
-                                    Shubhankar Consulting assists clients in growing and transforming their organizations by designing diverse and personalized business policies and procedures, and then integrating them with technology, strategy, and operational excellence.
-                                </p>
-                            </div>
-                        </div>
-                    </div>
-                </section>
+                <OurPurpose/>
                 <CoreValues />
                 <FeaturedClients/>
                 <ReimagineYourCareer/>
