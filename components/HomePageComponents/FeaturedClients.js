c214aefc2aa2215aec694c3e95009abc3cde54dc
SHAHJAHAN MD. SWAJAN <swajan.mis13.du@gmail.com>
2021-08-10 16:14:09 +0600

    color filter working well bro

--- components/HomePageComponents/FeaturedClients.js
+++ components/HomePageComponents/FeaturedClients.js
@@ -12,7 +12,12 @@ const FeaturedClients = () => {
 
                     </div>
                     <div className="row d-flex align-items-center">
-                        <FeaturedCompany img="/img/home/featured-clients/1.png" name="undp" />
+                        <div className="col-md-3 ">
+                            <div className="p text-center">
+                                <img src="/img/home/featured-clients/1.png" alt="undp" className="img-fluid" />
+                            </div>
+                        </div>
+                        {/* <FeaturedCompany img="/img/home/featured-clients/1.png" name="undp" /> */}
                         <FeaturedCompany img="/img/home/featured-clients/2.png" name="undp" />
                         <FeaturedCompany img="/img/home/featured-clients/3.png" name="undp" />
                         <FeaturedCompany img="/img/home/featured-clients/4.png" name="undp" />
@@ -22,7 +27,12 @@ const FeaturedClients = () => {
                         <FeaturedCompany img="/img/home/featured-clients/8.png" name="undp" />
                         <FeaturedCompany img="/img/home/featured-clients/9.png" name="undp" />
                         <FeaturedCompany img="/img/home/featured-clients/10.png" name="undp" />
-                        <FeaturedCompany img="/img/home/featured-clients/11.svg" name="undp" />
+                        <div className="col-md-3 ">
+                            <div className="p-3 text-center">
+                                <img src="/img/home/featured-clients/11.svg" alt="undp" className="img-fluid" />
+                            </div>
+                        </div>
+                        {/* <FeaturedCompany img="/img/home/featured-clients/11.svg" name="undp" /> */}
                         <FeaturedCompany img="/img/home/featured-clients/12.png" name="undp" />
                     </div>
                 </div>
@@ -37,7 +47,7 @@ const FeaturedCompany = ({ img, name }) => {
     return (
         <>
             <div className="col-md-3 ">
-                <div className="p-5 text-center">
+                <div className="px-5 py-4 text-center">
                     <img src={img} alt={name} className="img-fluid" />
                 </div>
             </div>
