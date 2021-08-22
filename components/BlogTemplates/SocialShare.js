09c058d7402310e5077966b1cd414149abe6796d
SHAHJAHAN MD. SWAJAN <swajan.mis13.du@gmail.com>
2021-08-22 22:16:39 +0600

    insight single responsive

--- components/BlogTemplates/SocialShare.js
+++ components/BlogTemplates/SocialShare.js
@@ -6,10 +6,10 @@ const PostSocialShare = ({link, className, docLink}) => {
     return (
         <>
 
-            <div className={"gray-bg w-100 p-4 round-6 "+ className}>
-                <div className="d-flex justify-content-between">
-                    <div className="d-flex align-items-center">
-                        <h5 className="font-weight-bold mr-4 mb-0">SHARE</h5>
+            <div className={"gray-bg w-100 p-4 my-4 my-md-0 round-6 "+ className}>
+                <div className="d-md-flex text-center justify-content-between">
+                    <div className="d-md-flex text-center align-items-center">
+                        <h5 className="font-weight-bold mr-4 mb-3 mb-md-0">SHARE</h5>
                         <div>
                             <LinkedinShareButton title="Swajan" url={link}> <div className="tutor-contact-icon-container" >
                                 <div className="tutor-contact-icons"><FaLinkedinIn /></div>
@@ -26,7 +26,7 @@ const PostSocialShare = ({link, className, docLink}) => {
                         </div>
                     </div>
                     <div>
-                        {docLink && <a href={docLink} className="btn btn-primary btn-lg" target="_blank" rel="noopener noreferrer">Read full document</a> }
+                        {docLink && <a href={docLink} className="btn btn-primary btn-lg mt-3 mt-md-0" target="_blank" rel="noopener noreferrer">Read full document</a> }
                     </div>
                 </div>
             </div>
